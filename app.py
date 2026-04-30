cbbded97b6ef255c11ec77731a7ac6c3f4d5d28c
test123929 <abderrahmanch@hotmail.com>
2026-05-01 00:49:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -507,7 +507,8 @@ DISCLAIMER = """
 </div>
 """
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     print("\n✅ NovaTech portaal gestart op http://localhost:5000\n")
     app.run(debug=True, host="0.0.0.0", port=5000)
